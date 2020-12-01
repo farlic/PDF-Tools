c9a1c2fb2f72cadaf6d1bc02f0d4efabbfe1464c
farlic <thelewisking@gmail.com>
2020-12-01 22:55:58 +0000

    added index support for easier page organisation

--- pdf_merger.py
+++ pdf_merger.py
@@ -6,6 +6,7 @@ positive_response = ['y','yes','ye','yeah','yep','sure','yeah','positive',"true"
 negative_response = ['n','no','nope','negative','ne','nep','nah','',None,'false'] # completely redundant but nice to have
 
 pdfs = [f for f in os.listdir() if f.endswith('.pdf') or f.endswith('.PDF')]
+lines = ''
 
 if 'output.pdf' in pdfs:
     os.remove('output.pdf')
@@ -16,13 +17,36 @@ if not pdfs:
     print('no pdfs found, exiting...')
     sys.exit()
 
-pdfs.sort(key=lambda x: int(x.split('.')[0][5:]))
+index = input('do you want to stort by index file? y/n ... ')
+
+if index in positive_response:
+    try:
+        with open('index.txt','r') as f:
+            lines = [line.strip() for line in f.readlines()]
+            
+    except FileNotFoundError:
+        print('no index found')
+        exit_ = input('... ')
+        sys.exit()
+
+else:
+    pdfs.sort(key=lambda x: int(x.split('.')[0][5:]))
+
+if lines:
+    for pos,line in enumerate(lines):
+        if line.isnumeric():
+            lines[pos] = f'page_{line}.pdf'
+        else:
+            lines[pos] = f'{line}.pdf'
+    pdfs = lines
+            
+
 
 show_pdfs = input('do you want to see PDFs detected? y/n ... ')
 if show_pdfs in positive_response:
     print(pdfs)
 
-purge = input('do you want to delete PDFs used? y/n ... ')
+purge = input('do you want to delete files used? y/n ... ')
 
 merger = PdfFileMerger(strict=False)
 for pdf in pdfs:
