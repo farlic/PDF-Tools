fa4e6f60f839617371bc62178d14ff8ade28d98d
farlic <thelewisking@gmail.com>
2020-12-01 20:43:16 +0000

    added optional index in file output and tidied up

--- pdf_splitter.py
+++ pdf_splitter.py
@@ -3,12 +3,15 @@
 
 from PyPDF2 import PdfFileReader
 from PyPDF2 import PdfFileWriter
-import os,sys
+import os,sys,math
 
 positive_response = ['y','yes','ye','yeah','yep','sure','yeah','positive',"true"]
 negative_response = ['n','no','nope','negative','ne','nep','nah','',None,'false'] # completely redundant but nice to have
 
 pdfs = [f for f in os.listdir() if f.endswith('.PDF') or f.endswith('.pdf')]
+
+if 'index.txt' in os.listdir():
+    os.remove('index.txt')
 
 if len(pdfs) > 1:
     print('too many files found')
@@ -29,23 +32,36 @@ purge = input('do you want to delete output PDF used? y/n ... ')
 pdf = pdfs[0] # select input pdf
 reader = PdfFileReader(pdf)
 
-i=0
-for pagenum in range (reader.getNumPages()):
+index = input('do you want to save a index card? y/n ... ')
+
+for pagenum in range(reader.getNumPages()):
     if double_scan in positive_response and not pagenum %2 ==0:
-        print('page skipped')
         continue
 
     page = reader.getPage(pagenum)
 
     writer = PdfFileWriter()
     writer.addPage(page)
-    if double_scan in positive_response:
-        page2 = reader.getPage(pagenum+1)
-        writer.addPage(page2)
 
-    i += 1
-    with open(f'page_{i}.pdf','wb') as out:
+    try:
+        if double_scan in positive_response:
+            page2 = reader.getPage(pagenum+1)
+            writer.addPage(page2)
+
+    except:
+        pass
+
+    with open(f'page_{pagenum+1}.pdf','wb') as out:
         writer.write(out)
+
+if index in positive_response:
+    with open('index.txt','w') as write_index:
+        if double_scan in positive_response:
+            index_ = [str(i) for i in range(1,math.floor(reader.getNumPages()/2)+1)]
+        else:
+            index_  = [str(i) for i in range(1,reader.getNumPages()+1)]
+        write_index.write('\n'.join(index_))
+
 
 if purge in positive_response:
     os.remove(pdf)
